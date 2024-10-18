272d8d7b8e0f3d7bb072457b1115531b5c7bfe5c
valentine <150450259+valbuildr@users.noreply.github.com>
2024-10-18 14:53:27 -0500

    apple touch icon + removing unused file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,9 @@
     <meta name="og:image" content="/linksvalbuildsxyz.png" />
     <!-- google fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
+    <!-- apple touch icon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180.png" />
 </head>
 
 <script>
