79e90f14c3efe77d97dc3bc5465f7d35c9ca995e
valentine <150450259+valbuildr@users.noreply.github.com>
2024-10-18 19:54:58 -0500

    fix arialabels for custom icons

--- src/lib/MastodonSolid.svelte
+++ src/lib/MastodonSolid.svelte
@@ -52,7 +52,7 @@
     } else {
       hasDescription = false;
     }
-    export let ariaLabel = 'apple solid';
+    export let ariaLabel = 'mastodon solid';
   </script>
   
   {#if withEvents}
--- src/lib/TikTokSolid.svelte
+++ src/lib/TikTokSolid.svelte
@@ -52,7 +52,7 @@
     } else {
       hasDescription = false;
     }
-    export let ariaLabel = 'apple solid';
+    export let ariaLabel = 'tiktok solid';
   </script>
   
   {#if withEvents}
