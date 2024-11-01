d9f793024f102864b1f02ae20f38ef15de95dd8b
valentine <150450259+valbuildr@users.noreply.github.com>
2024-11-01 14:56:57 -0500

    v2.0

--- src/routes/discord/+page.svelte
+++ src/routes/discord/+page.svelte
@@ -1,24 +1,25 @@
 <script>
-    const url = "https://discord.gg/UKVMvUG2h9";
+  const url = "https://discord.gg/UKVMvUG2h9";
 
-    import { browser } from '$app/environment';
-    
-    if (browser) {
-        window.location.href = url;
-    }
+  import { browser } from "$app/environment";
 
-    import { Spinner } from "flowbite-svelte";
+  if (browser) {
+    window.location.href = url;
+  }
 </script>
 
 <head>
-    <!-- title, description, and image for embeds -->
-    <title>valentine's discord server</title>
-    <meta name="og:title" content="valentine's discord server" />
-    <meta name="twitter:card" content="/linksvalbuildsxyz.png" />
+  <!-- title, description, and image for embeds -->
+  <title>valentine's discord server</title>
+  <meta name="og:title" content="valentine's discord server" />
+  <meta name="twitter:card" content="/linksvalbuildsxyz.png" />
 </head>
 
 <div class="text-center">
-    <h5 class="mt-2 text-xl font-medium text-gray-900 dark:text-white pb-2">Redirecting...</h5>
-    <Spinner size={8} />
-    <h6 class="text-md text-gray-700 dark:text-gray-300">If it isn't working, click <a href={url} class="hover:underline">here</a>.</h6>
+  <h5 class="mt-2 text-xl font-medium text-black dark:text-white pb-2">
+    Redirecting...
+  </h5>
+  <h6 class="text-md text-gray-500 dark:text-gray-400">
+    If it isn't working, click <a href={url} class="hover:underline">here</a>.
+  </h6>
 </div>
--- src/routes/steam/+page.svelte
+++ src/routes/steam/+page.svelte
@@ -6,8 +6,6 @@
     if (browser) {
         window.location.href = url;
     }
-
-    import { Spinner } from "flowbite-svelte";
 </script>
 
 <head>
@@ -18,7 +16,10 @@
 </head>
 
 <div class="text-center">
-    <h5 class="mt-2 text-xl font-medium text-gray-900 dark:text-white pb-2">Redirecting...</h5>
-    <Spinner size={8} />
-    <h6 class="text-md text-gray-700 dark:text-gray-300">If it isn't working, click <a href={url} class="hover:underline">here</a>.</h6>
-</div>
+    <h5 class="mt-2 text-xl font-medium text-black dark:text-white pb-2">
+      Redirecting...
+    </h5>
+    <h6 class="text-md text-gray-500 dark:text-gray-400">
+      If it isn't working, click <a href={url} class="hover:underline">here</a>.
+    </h6>
+  </div>
