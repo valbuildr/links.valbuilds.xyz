f7d0bbf38bf29302a838867330e5a6c079931368
valentine <150450259+valbuildr@users.noreply.github.com>
2024-11-01 15:30:49 -0500

    move all links to new button component and change contrast between border/hover fill of buttons in light mode

---
+++ src/lib/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a
+  {href}
+  class="flex gap-2 items-center p-2 border border-dark-mint dark:border-mint hover:bg-dark-mint dark:hover:bg-mint hover:text-white hover:fill-white hover:dark:text-black hover:dark:fill-black"
+  target="_blank"
+>
+  <slot />
+</a>
