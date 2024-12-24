57deacb9e8472e22487111282188aadf4dc0f44f
valentine <150450259+valbuildr@users.noreply.github.com>
2024-12-24 00:54:28 -0600

    Add target prop for button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   export let href: string;
+  export let target: string = "_blank";
 </script>
 
 <a
   {href}
   class="flex gap-2 items-center p-2 border border-dark-mint dark:border-mint hover:bg-dark-mint dark:hover:bg-mint hover:text-white hover:fill-white hover:dark:text-black hover:dark:fill-black"
-  target="_blank"
+  {target}
 >
   <slot />
 </a>
