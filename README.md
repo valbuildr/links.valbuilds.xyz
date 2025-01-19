09c57bbea93d6fc01dc5f3ce265e90238997680f
valentine <150450259+valbuildr@users.noreply.github.com>
2025-01-18 18:03:37 -0600

    redirect to main website

--- README.md
+++
@@ -1,5 +0,0 @@
-# links.valbuilds.xyz
-
-A simple website built in Svelte that links to some of my social media profiles.
-
-This repository is licensed under the [MIT License](./LICENSE).
--- src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-export { default as BlueskySolid } from "./BlueskySolid.svelte";
-export { default as MastodonSolid } from "./MastodonSolid.svelte";
-export { default as TikTokSolid } from "./TikTokSolid.svelte";
-export { default as SteamSolid } from "./SteamSolid.svelte";
-export { default as PronounsPageSolid } from "./PronounsPageSolid.svelte";
-export { default as TwitchSolid } from "./TwitchSolid.svelte";
-export { default as Button } from "./Button.svelte";
